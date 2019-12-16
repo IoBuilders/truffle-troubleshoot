532ae92a33f7c2baf5ca892f895e0a1ad68a42a3
Daniel Lehrner <daniel@io.builders>
2019-12-16 23:13:36 +0100

    refactored revert reason to be class based

--- src/index.js
+++ src/index.js
@@ -1,65 +1,67 @@
 const chalk = require('chalk')
-const Web3 = require('web3')
 
-const revertReason = require('./revert-reason')
+const COMMANDS = {
+  'revert-reason': require('./commands/revert-reason')
+}
 
 let exitCallBack
+let helpText
 
 function exitWithError (message) {
   exitCallBack(chalk.bold.red(message))
 }
 
-function printHelp () {
-  console.log(
-    '\nUsage: truffle run troubleshoot [options] <command> [commandArguments...] \n\n' +
-    'A Truffle plugin to troubleshoot your smart contract problems\n\n' +
-    // 'Options:\n' +
-    // '  -v, --version                    output the version number\n' +
-    // '  -h, --help                       Prints this help information\n\n' +
-    'Commands:\n' +
-    '  revert-reason <transactionHash>  Get the revert reason of a failed transaction\n'
-  )
+function constructHelpText () {
+  helpText = '\nUsage: truffle run troubleshoot [options] <command> [commandArguments...] \n\n' +
+             'A Truffle plugin to troubleshoot your smart contract problems\n\n' +
+             // 'Options:\n' +
+             // '  -v, --version                    output the version number\n' +
+             '  -h, --help                       Prints this help information\n\n' +
+             'Commands:\n'
+
+  for (const [commandName, commandObject] of Object.entries(COMMANDS)) {
+    helpText += `  ${commandName} ${commandObject.getHelpText()}\n`
+  }
 }
 
-module.exports = async (config, done) => {
+module.exports = async (truffleConfig, done) => {
   exitCallBack = done
 
-  if (config.network === undefined) {
-    config.network = 'development'
+  if (truffleConfig.network === undefined) {
+    truffleConfig.network = 'development'
   }
 
-  if (config.help) {
-    printHelp()
+  constructHelpText()
+
+  if (truffleConfig.help) {
+    console.log(helpText)
     exitCallBack()
   }
 
-  if (config._.length === 1) {
-    printHelp()
+  if (truffleConfig._.length === 1) {
+    console.log(helpText)
     exitWithError('No command given')
   }
 
-  const command = config._[1]
-  // process.argv is used here, because config._ seems to convert the arguments in unpredictable ways
-  // e.g. a transaction hash is returned as Number object
-  const commandArguments = process.argv.splice(5)
+  const commandName = truffleConfig._[1]
 
-  const web3 = new Web3(config.provider)
-  let result
+  if (!(commandName in COMMANDS)) {
+    exitWithError(`Unknown command ${commandName}`)
+  }
+
+  const command = COMMANDS[commandName].fromTruffle(
+    truffleConfig,
+    // process.argv is used here, because config._ seems to convert the arguments in unpredictable ways
+    // e.g. a transaction hash is returned as Number object
+    process.argv.splice(5)
+  )
 
   try {
-    switch (command) {
-      case 'revert-reason':
-        result = await revertReason(web3, config, ...commandArguments)
-        break
-      default:
-        printHelp()
-        exitWithError(`${command} is not a valid command`)
-    }
+    const result = await command.execute()
+    console.log(`\n${result}\n`)
   } catch (error) {
     exitWithError(error)
   }
 
-  console.log(`\n${result}\n`)
-
-  done()
+  exitCallBack()
 }
